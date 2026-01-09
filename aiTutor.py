ad6f04c50dc58b59cdb8de3dcbf316f47f77335b
Massad Raza <massad.raza@hotmail.com>
2026-01-09 14:49:05 -0500

    Built a functional RAG pipeline

--- aiTutor.py
+++ aiTutor.py
@@ -110,3 +110,84 @@ def save_memory_node(state: TutorState):
 
 
 # Building the graph
+def decide_next_step(state: TutorState):
+    """Decides whether to give a hint or save to memory"""
+    feedback = state.get("user_feedback", "")
+
+    if feedback == "hint":
+        print("[Router] User wants hint -> going to Hint Node")
+        return "hint"
+    else:
+        print("[Router] Saving and ending -> going to Save Memory")
+        return "save"
+    
+workflow = StateGraph(TutorState)
+
+workflow.add_node("retriever", retriever_node)
+workflow.add_node("llm", llm_node)
+workflow.add_node("hint", hint_node)
+workflow.add_node("save", save_memory_node)
+
+workflow.set_entry_point("retriever")
+
+workflow.add_edge("retriever", "llm")
+workflow.add_conditional_edges(
+    "llm",
+    decide_next_step,
+    {
+        "hint": "hint",
+        "save": "save"
+    }
+)
+
+workflow.add_edge("hint", "save")
+
+workflow.add_edge("save", END)
+
+app = workflow.compile()
+
+
+# Main Loop
+
+
+print("\n🎓 Welcome to your AI Tutor!")
+print("Ask questions about your study guide.")
+print("After each answer, type 'hint' for a hint, or press Enter to continue.")
+print("Type 'exit' to quit.\n")
+
+chat_history = []
+
+while True:
+    query = input("You: ")
+
+    if query.lower() in ["exit", "quit"]:
+        print("Goodbye! Happy Studying!")
+        break
+
+    result = app.invoke({
+        "question": query,
+        "chat_history": chat_history,
+        "retrieved_docs": [],
+        "answer": "",
+        "user_feedback": ""
+    })
+
+    print(f"\n AI Tutor:  {result['answer']} \n")
+
+    chat_history = result["chat_history"]
+
+    feedback = input("Type 'hint' for a hint, or press Enter to continue: ")
+
+    if feedback.lower() == "hint":
+        result = app.invoke({
+            "question": query,
+            "chat_history": chat_history[:-2],
+            "retrieved_docs": result["retrieved_docs"],
+            "answer": "",
+            "user_feedback": "hint"
+        })
+
+        print(f"\n Hint: {result['answer']}")
+
+        chat_history = result["chat_history"]
+
