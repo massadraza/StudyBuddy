9e3784c0f5f49ce3f58570a2a79a1811dd04a24c
Massad Raza <massad.raza@hotmail.com>
2026-01-08 22:50:55 -0500

    Defining a schema

--- aiTutor.py
+++ aiTutor.py
@@ -1,6 +1,34 @@
 from typing import TypedDict, List, Annotated
 from langchain_core import BaseMessage
 import operator
+import os
+from dotenv import load_dotenv
+
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.chat_models import ChatOpenAI
+from langchain_core.messages import HumanMessage, AIMessage, BaseMessage
+from langchain_core.prompts import ChatPromptTemplate
+
+from langgraph.graph import StateGraph, END
+
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+if not OPENAI_API_KEY:
+    raise ValueError("OPENAI_API_KEY not found in .env file")
+
+with open("study_guide.txt", "r", encoding="utf-8") as f:
+    text = f.read()
+
+text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+chunks = text_splitter.split_text(text)
+embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+vectorstore = FAISS.from_texts(chunks, embedding=embeddings)
+
+llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, temperature=0)
 
 class TutorState(TypedDict):
     question: str
