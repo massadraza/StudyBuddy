b976b240c26b15160b75d0943e914aaa06a7c9c5
Massad Raza <massad.raza@hotmail.com>
2026-01-17 18:28:43 -0500

    Added some comments 3

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -25,7 +25,17 @@ If we didn't have dependency injection
 
 EXAMPLE:
 
+def get_users():
+    db = SessionLocal()
+    users = db.query(User).all()
+    db.close()
+    return users
 
+WITH DEPENDENCY INJECTION
+
+def get_users(db: Session = Depends(get_db)):
+    users = db.query(Users).all()
+    return users
 
 """
 
