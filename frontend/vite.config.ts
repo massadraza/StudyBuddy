7dd761b70f97e4369f39615e9d6f62c0582815f1
Massad Raza <massad.raza@hotmail.com>
2026-01-18 19:06:49 -0500

    Add host

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      "independent-illumination-production-4d0f.up.railway.app",
+    ]
+  },
 })
