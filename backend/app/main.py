453010b5cd4b27097aeb0b531da9afdbc220b7ff
Massad Raza <massad.raza@hotmail.com>
2026-01-18 19:09:58 -0500

    Updated CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -32,8 +32,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.cors_origins_list,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allow_headers=["Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With"]
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Include routers 
